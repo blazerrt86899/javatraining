d3f5ad75d17e014dcec30352810bd471138f4b1f
Romu Tiwari <35737000+blazerrt86899@users.noreply.github.com>
2020-01-08 23:50:01 +0530

    Question 4 Student 08/01/2020

--- DayScholar.java
+++ DayScholar.java
@@ -1,3 +1,9 @@
+/* Author: Romu Tiwari
+ * Employee id: 844549
+ * Created on: 08/01/2020
+ * Time: 11:00pm
+ * Description: Test Inheritance and Overriding
+ * */
 package com.cts.day2;
 
 public class DayScholar extends Student{
