1379ac6f8a0657a1e4fb909db51112e11b8bab83
Romu Tiwari <35737000+blazerrt86899@users.noreply.github.com>
2020-01-13 00:44:46 +0530

    LinkedList VS Vector

---
+++ TestVector.java
@@ -0,0 +1,20 @@
+package com.cts.javaDay5;
+
+import java.util.*; 
+
+public class TestVector { 
+    public static void main(String[] arg) 
+    { 
+  
+        // create default vector 
+        Vector v = new Vector(); 
+  
+        v.add(1); 
+        v.add(2); 
+        v.add("geeks"); 
+        v.add("forGeeks"); 
+        v.add(3); 
+  
+        System.out.println("Vector is " + v); 
+    } 
+}
